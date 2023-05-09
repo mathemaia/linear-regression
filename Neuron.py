2359844604c827dfcdacf765476606db2bc9eb90
mathemaia <matheusmaiadasilva@gmail.com>
2023-05-09 15:18:32 -0300

    update

--- Neuron.py
+++ Neuron.py
@@ -10,11 +10,12 @@ class Neuron:
         self.predict = 0.0
         self.error = 0.0
         self.N = N
+        self.x = 0
 
     # método que calcula a predição
     def forward(self, x) -> float:
         self.x = x
-        self.predict = self.w * x + self.b
+        self.predict = self.w * self.x + self.b
 
     # método que calcula as derivadas e atualiza as variáveis
     def backward(self) -> None:
@@ -22,11 +23,11 @@ class Neuron:
         db = (1 / self.N) * self.error
 
         self.w = self.w - self.lr * dw
-        self.b = self.b - self.lr * db
+        #self.b = self.b - self.lr * db
     
     # função que calcula o erro
     def MSE(self, y) -> float:
-        self.error = self.predict - y
+        self.error = (self.predict - y) ** 2
 
     
     
